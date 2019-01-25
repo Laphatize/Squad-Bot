322c8c0e96a4733cd2c7d5ba2b169c7ce5e96291
Pranav Ramesh <laphatize.me@gmail.com>
2019-01-24 22:54:37 -0500

    Updated index.js
    Added the following commands:
    + Custom Greetings
    + Donation System
    
    Other Updates:
    + Fixed a bug with the role add system.
    + Started implementing a database system.  See Issue #1 (https://github.com/Laphatize/Squad-Bot/issues/1)

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var config = require('./config.json');
+var responses = ["blank", "yello", "howdy", "hola", "wassup", "hi" , "hello", "hoi", "bonjour"];
+
+
 
 client.on('ready', () => {
     console.log("I have started.")
@@ -46,6 +49,9 @@ client.on('message', message => {
         message.channel.send("Registering " + message.author.username + " for " + role + "...");
         member.addRole(role);
         message.channel.send(message.author.username + " is now registered to get updates for " + role);
+		
+		//var server = message.guild.id;
+		
         client.channels.get("538132739749052431").send(message.author.username + " signed up for " + role + "!");
 
     }
@@ -61,6 +67,30 @@ client.on('message', message => {
 			message.reply("You don't have the permissions to execute this command.");
 		}
     }
+	
+	if (message.content.startsWith("!donate")) {
+		message.channel.send('Do you want to donate to Laphatize?')
+		.then(() => {
+		  message.channel.awaitMessages(response => response.content === 'yes', {
+			max: 1,
+			time: 5000,
+			errors: ['time'],
+		  })
+		  .then((collected) => {
+		
+			  message.channel.send(`Cool! Here is his patron https://www.patreon.com/join/2368589/signup.`);
+			
+		  }).catch(() => {
+			  message.channel.send('Cancelled!');
+			});
+		});
+	}
+	
+	if (message.content === "!hi") {
+		var x = Math.floor((Math.random() * 8) + 1);
+		message.reply(responses[x]);
+	}
+
 
 });
 
